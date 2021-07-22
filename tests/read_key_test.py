26dbf2f09145f98782827e62dfe4e8dfb28d1695
sudo-julia <jlearning@tuta.io>
2021-07-22 12:32:25 -0700

    test imports working

--- tests/read_key_test.py
+++ tests/read_key_test.py
@@ -2,11 +2,9 @@
 """test read_key"""
 from pathlib import Path
 from tempfile import NamedTemporaryFile
-import pytest
 from configmanager import read_key
 
 
-@pytest.mark.skip
 def test_read_key():
     """test read_key"""
     with NamedTemporaryFile("w+", suffix=".txt") as tmpf:
